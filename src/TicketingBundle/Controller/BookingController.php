49f0c9063aae44f806177140a616a3576f26c755
chris7862 <leroc-h.christian@orange.fr>
2018-09-06 19:21:48 +0200

    AppBundle delete, routing and service modification and simple layout creation

--- src/TicketingBundle/Controller/BookingController.php
+++ src/TicketingBundle/Controller/BookingController.php
@@ -2,17 +2,19 @@
 
 // src/TicketingBundle/Controller/BookingController.php
 
-namespace CL\TicketingBundle\Controller;
+namespace TicketingBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class BookingController extends Controller
 {
+    /**
+     * @Route("/booking")
+     */
     public function indexAction()
     {
-        $content = $this->get('templating')->render('TicketingBundle:Booking:index.html.twig');
-
-        return new Response($content);
+        return $this->render('booking/index.html.twig');
     }
 }

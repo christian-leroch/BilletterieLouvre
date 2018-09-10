6224d37b2453f432e4bccaaee418491a9e017263
chris7862 <leroc-h.christian@orange.fr>
2018-09-10 18:33:40 +0200

    BookingController, index and layout views modification

--- src/TicketingBundle/Controller/BookingController.php
+++ src/TicketingBundle/Controller/BookingController.php
@@ -5,8 +5,13 @@
 namespace TicketingBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Routing\Annotation\Route;
+use TicketingBundle\Entity\Booking;
 
 class BookingController extends Controller
 {
@@ -15,6 +20,32 @@ class BookingController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('booking/index.html.twig');
+        $booking = new Booking();
+        $booking->setDate(new \DateTime('now'));
+        $booking->setTicketType(false);
+        $booking->setNbTicket('1');
+
+        $form = $this->createFormBuilder($booking)
+            ->add('date',DateType::class , array(
+                    'label' => '1 . Choisissez la date de votre visite',
+                    'widget' => 'single_text',
+                    'attr' => ['id' => ''])
+            )
+            ->add('ticketType', ChoiceType::class, array(
+                    'label' => '2 . Choisissez votre horaire d\'accès au musée',
+                    'choices' => array('Journée' => 'Journee', 'Demi-journée (accès au musée à partir de 14h00)' => 'Demi-journee'),
+                    'expanded'  => true,
+                    'multiple'  => false,)
+            )
+            ->add('nbTicket', IntegerType::class, array(
+                    'label' => 'Nombre de billets ',
+                    'data' => 1,
+                    'attr' => array('min'=> 1, 'max' => 1000),)
+            )
+            ->getForm();
+
+        return $this->render('booking/index.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
